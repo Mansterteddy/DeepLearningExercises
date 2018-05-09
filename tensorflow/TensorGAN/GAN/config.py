8a56e190d357d05002b01d5edd829bad62c9cf76
Zhang Yuan <zhangyuan@ict.ac.cn>
2018-05-09 16:14:00 +0800

    Update raw GAN

---
+++ tensorflow/TensorGAN/GAN/config.py
@@ -0,0 +1,7 @@
+# Training hyper parameters
+
+num_steps = 1000
+hidden_size = 4
+batch_size = 8
+minibatch = True
+log_every = 10
---
+++ tensorflow/TensorGAN/GAN/main.py
@@ -0,0 +1,20 @@
+import net
+import util
+import config
+
+import tensorflow as tf
+import seaborn as sns
+import numpy as np
+
+sns.set(color_codes=True)
+
+seed = 42
+np.random.seed(seed)
+tf.set_random_seed(seed)
+
+def main():
+    gan = net.GAN(config)
+    net.train(gan, util.DataDistribution(), util.GeneratorDistribution(range=8), config)
+
+if __name__=="__main__":
+    main()
