f25bbe17b6e4b04871d013ce56bd964d8ac49fcc
Zhang Yuan <zhangyuan@ict.ac.cn>
2018-05-14 09:46:32 +0800

    Update 0514, add CGAN.

--- tensorflow/TensorGAN/cGAN/net.py
+++ tensorflow/TensorGAN/cGAN/net.py
@@ -1,13 +1,1 @@
 import tensorflow as tf
-
-def xavier_init(size):
-    in_dim = size[0]
-    xavier_stddev = 1. / tf.sqrt(in_dim / 2.)
-    return tf.random_normal(shape=size, stddev=xavier_stddev)
-
-# Disriminator Net
-
-
-# Generator Net
-
-def gene
