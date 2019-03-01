d783be84e13031a6026ab4dcfc16ad2e94b03b35
Yuan Zhang (RELEVANCE) <yuazhan@microsoft.com>
2019-03-01 12:30:24 +0800

    Add word2vec and LSTM.

---
+++ pytorch/NLP/WordEmbeddings/cbow.py
@@ -0,0 +1,33 @@
+CONTEXT_SIZE = 2 # 2 words to the left, 2 to the right.
+raw_text = """We are about to study the idea of a computational process.
+Computational processes are abstract beings that inhabit computers.
+As they evolve, processes manipulate other abstract things called data.
+The evolution of a process is directed by a pattern of rules
+called a program. People create programs to direct processes. In effect,
+we conjure the spirits of the computer with our spells.""".split()
+
+# By deriving a set from `raw_text`, we deduplicate the array
+vocab = set(raw_text)
+vocab_size = len(vocab)
+
+word_to_ix = {word: i for i, word in enumerate(vocab)}
+data = []
+for i in range(2, len(raw_text) - 2):
+    context = [raw_text[i - 2], raw_text[i - 1],
+               raw_text[i + 1], raw_text[i + 2]]
+    target = raw_text[i]
+    data.append((context, target))
+print(data[:5])
+
+class CBOW(nn.Module):
+    def __init__(self):
+        pass
+    
+    def forward(self, inputs):
+        pass
+
+def make_context_vector(context, word_to_ix):
+    idxs = [word_to_ix[w] for w in context]
+    return torch.tensor(idxs, dtype=torch.long)
+
+make_context_vector(data[0][0], word_to_ix)
