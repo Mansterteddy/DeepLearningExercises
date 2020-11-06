e67d6a6c4d32857d0f76da8d5a1c774cf8da40b2
Yuan Zhang (RELEVANCE) <yuazhan@microsoft.com>
2020-11-06 12:18:54 +0800

    Add Optim.

---
+++ pytorch/Basic/Optim/optim.py
@@ -0,0 +1,21 @@
+import torch
+
+x_init = torch.randn(2)
+x = x_init.clone()
+x.requires_grad = True
+
+optimizer = torch.optim.SGD([x], lr=0.1, momentum=0.5)
+
+bowl = torch.tensor([[0.4410, -1.0317], [-0.2844, -0.1035]])
+track, losses = [], []
+
+for iter in range(21):
+    objective = torch.mm(bowl, x[:, None]).norm()
+    optimizer.zero_grad()
+    objective.backward()
+    optimizer.step()
+    track.append(x.detach().clone())
+    losses.append(objective.detach())
+
+print(track)
+print(losses)
